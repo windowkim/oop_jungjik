f38c1aa34a87daa723446dc870a342bbb6946e23
chang <windowkim@postech.ac.kr>
2018-05-26 06:13:14 +0900

    making time

--- app/src/main/java/com/total/administor/oop_jungjik/WatchCheck.java
+++ app/src/main/java/com/total/administor/oop_jungjik/WatchCheck.java
@@ -1,8 +1,12 @@
 package com.total.administor.oop_jungjik;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -21,15 +25,27 @@ public class WatchCheck extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_watch_check);
 
+        Typeface typeface = Typeface.createFromAsset(getAssets(),"fonts/BMJUA_ttf.ttf");
+
         contents = (ArrayList<Content>) getIntent().getSerializableExtra("Contents");
 
-        LinearLayout
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.watchLinearLayout);
+
 
         for(int i=0;i<contents.size();i++)
         {
             TextView text = new TextView(this);
             text.setTag(i);
-            text.setText(contents.get(i).getName());
+            text.setTypeface(typeface);
+            text.setTextSize(27);
+            text.setGravity(Gravity.CENTER);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            params.setMargins(100,20,0,0);
+
+            text.setText(contents.get(i).getName()+"			"+Integer.toString(contents.get(i).getMinute())+"분 "+Integer.toString(contents.get(i).getSeond())+"초");
+            text.setTextColor(Color.BLACK);
+            text.setLayoutParams(params);
+            linearLayout.addView(text,i);
 
         }
 
